33968ebd72e5711693b6560f594b96658fe794d9
StudioLee-cmd <tim@studiolee.nl>
2026-03-05 01:02:56 +0100

    Fix siteDetails: niche field, live siteUrl, siteName, language nl-NL; FreeTrialModal uses niche

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,11 +1,12 @@
 export const siteDetails = {
     siteName: 'ElektroAI',
-    siteUrl: 'https://elektroai.nl/',
+    niche: 'Elektriciens',
+    siteUrl: 'https://www.elektroai.nl/',
     metadata: {
         title: 'ElektroAI - De slimme assistent voor jouw elektriciensbedrijf',
         description: 'Bespaar zeeën van tijd en zeg vaarwel tegen saai kantoorwerk. ElektroAI regelt je agenda, telefoon en administratie volledig automatisch.',
     },
-    language: 'nl-nl',
+    language: 'nl-NL',
     locale: 'nl-NL',
     siteLogo: '/images/logo-transparent.png',
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
